72ea51a5780a8a32a55cc2118d2d643b098c1a12
anastasiiakorinkova <114064846+anastasiiakorinkova@users.noreply.github.com>
2022-09-27 23:35:06 +0300

    Create cv2.c

---
+++ cv2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    float priemer_kolesa, vzdialenost_trasy, result;
+    float palec_cm = 2.54, pi = 3.14;
+    
+    printf ("Zadajte priemer kolesa v palcoch: ");  
+    scanf ("%f", &priemer_kolesa);
+    
+    printf ("Zadajte vzdialenost v km: ");  
+    scanf ("%f", &vzdialenost_trasy);
+    
+    float vzdialenost_kolesa = pi * priemer_kolesa * palec_cm / 100000;
+    
+    result = vzdialenost_trasy / vzdialenost_kolesa;
+
+    printf ("Koleso sa na ceste otoci %f krat.", result);
+
+    return 0;
+}
