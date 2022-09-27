d54ac69f98b353286669b8ff5b2b881c35a05418
anastasiiakorinkova <114064846+anastasiiakorinkova@users.noreply.github.com>
2022-09-27 22:45:32 +0300

    Create cv1.c

---
+++ cv1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float fahr, result;
+    
+    printf ("Zadajte pocet stupnov Fahrenheita: ");  
+    scanf ("%f", &fahr);
+    
+    result = (fahr - 32) * 5 / 9;
+
+    
+    printf ("%f Fahrenheita je %f Celsius", fahr, result);
+
+    return 0;
+}
