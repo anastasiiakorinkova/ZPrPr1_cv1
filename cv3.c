84a8bfffbb63cd6c7dc8caed63af046d479ddc1b
anastasiiakorinkova <114064846+anastasiiakorinkova@users.noreply.github.com>
2022-10-02 21:02:09 +0300

    Update cv3.c

--- cv3.c
+++ cv3.c
@@ -1,21 +1,25 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 #include <math.h>
 
 int main() {
- 
-    float a, b, c, x, x1, x2, discriminant_sqrt;
-   
-    printf ("Zadajte cleny v rovnici v tvare a b c: ");
-    scanf ("%f %f %f", &a, &b, &c);
-   
-    discriminant_sqrt = sqrt(pow(b, 2) - 4 * a * c);
-    x1 = (-b + discriminant_sqrt) / (2 * a);
-    x2 = (-b - discriminant_sqrt) / (2 * a);
-   
-    printf ("Korene kvadratickey rovnice su: \n");
-    printf ("x1 = %f\n", x1);
-    printf ("x2 = %f\n", x2);
-
+     float polomer, obvod_kruznice, obsah_kruhu, objem_gule, povrch_gule;
+     const float pi = 3.14;
+     const float  pie=3.14; 
+     
+     printf ("Zadajte polomer: ");
+     scanf ("%f", &polomer);
+     
+     obvod_kruznice = 2 * pi * polomer;
+     obsah_kruhu = pi * pow(polomer, 2);
+     objem_gule = (4 * pi * pow(polomer, 3)) / 3;
+     double  volume=(4.0/3.0)*pie*pow(polomer, 3);
+     povrch_gule = 4 * pi * pow(polomer, 2);
+     
+     printf ("Obvod kruznice = %f \n", obvod_kruznice);
+     printf ("Obsah kruhu = %f \n", obsah_kruhu);
+     printf ("Objem gule = %f \n", objem_gule);
+     printf ("Objem gule1 = %f \n", volume);
+     printf ("Povrch gule = %f \n", povrch_gule);
+     
     return 0;
 }
